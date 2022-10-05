62ecbd5851da2ca4ce61791f7ed20b791a39fb4c
sawyerrick <1103031078@qq.com>
2022-10-05 17:40:04 +0800

    init conmit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    indent: ['error', 2],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
---
+++ pages/env.js
@@ -0,0 +1,3 @@
+
+const APP_ID = process.env.APP_ID
+const APP_KEY = Od9lqhZuSThU7AilwYrN4UH7
