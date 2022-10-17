046eab4d05a44ed45440f084f8ca1ff88d93292e
sawyerrick <1103031078@qq.com>
2022-10-17 22:53:28 +0800

    Polish

--- pages/msg/index.jsx
+++ pages/msg/index.jsx
@@ -169,18 +169,26 @@ function CreateConversationWindow ({ visiable, setVisiable }) {
     <>
       <div className='inset-0 fixed bg-gray-900 opacity-25' onClick={() => setVisiable(false)}></div>
       {/* Align center */}
-      <div className='fixed mt-24 left-0 right-0 mx-auto z-10 w-full max-w-md'>
-        <div className='flex-col border border-gray-600 bg-white shadow-lg rounded-xl p-3'>
+      <div className='fixed mt-24 left-0 right-0 mx-auto z-10 w-full max-w-sm'>
+        <div className='flex-col border border-gray-600 bg-white shadow-lg rounded-xl p-3 '>
           <div className='flex items-center justify-between'>
             <h1>Create A Conversation</h1>
             <div className='hover:cursor-pointer' onClick={() => setVisiable(false)}>
               <AiOutlineClose />
             </div>
           </div>
-          <div className='bg-white mt-7 flex justify-center flex-col items-center'>
-            <input className="bg-gray-200 h-12 w-1/2 p-3 rounded-md" placeholder='Remote username'/>
-            <button className='w-36 h-12 rounded-full bg-sky-500 text-white hover:bg-sky-600 m-3 px-3 shrink-0 focus:ring focus:ring-sky-300 active:bg-sky-700 focus:outline-none'>Create</button>
+          <div>
+            <div className='bg-white mt-5 flex justify-between items-center'>
+              <input className="bg-gray-200 h-12 w-2/3 p-3 rounded-md" placeholder='Remote username'/>
+              <button className='w-24 h-10 rounded-full bg-sky-500 text-white hover:bg-sky-600 px-3 shrink-0 focus:ring focus:ring-sky-300 active:bg-sky-700 focus:outline-none'>Create</button>
+            </div>
+            {/* Create Conversation Status */}
+            <div className='flex mt-2 items-center mx-auto w-24'>
+              <div className='w-2 h-2 rounded-2xl bg-red-600'></div>
+              <p className="text-slate-500 mx-1">Offline</p>
+            </div>
           </div>
+
         </div>
       </div>
     </>
