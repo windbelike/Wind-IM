01d942cb464a3caaec09e23f2b6fe7e56cbc10e4
sawyerrick <1103031078@qq.com>
2023-02-10 23:25:07 +0800

    add friend window

--- src/pages/home/friend/AddFriendWindow.jsx
+++ src/pages/home/friend/AddFriendWindow.jsx
@@ -1,12 +1,22 @@
+import { useRef } from 'react'
+import { useMutation } from 'react-query'
 
 // todo 添加动效
 export default function AddFriendWindow ({ openAddFriendWindow, setOpenAddFriendWindow }) {
   const bgElementId = 'addFriendWindow'
+  // const loginMutation = useMutation(login)
+  const $email = useRef(null)
+
   function onClickCloseWindow (e) {
     if (e.target.id === bgElementId) {
       setOpenAddFriendWindow(false)
     }
   }
+
+  function onClickSubmit () {
+    console.log('input email:' + $email.current.value)
+  }
+
   if (!openAddFriendWindow) {
     console.log(openAddFriendWindow)
     return <></>
@@ -14,10 +24,14 @@ export default function AddFriendWindow ({ openAddFriendWindow, setOpenAddFriend
 
   return (
     <div id={bgElementId} className='fixed flex items-center z-20 w-full h-full left-0 top-0 bg-[rgba(0,0,0,0.5)]' onClick={onClickCloseWindow}>
-      <div className="mx-auto w-[440px] h-[231px] bg-[#25262a] rounded-2xl p-5 ">
+      <div className="mx-auto flex flex-col w-[440px] h-[231px] bg-[#25262a] rounded-2xl p-5 ">
         <h1 className="text-white text-xl font-bold">添加好友</h1>
         <p className="text-[#717579] mt-2">需要用户邮箱</p>
-        {/* <input type="text" className="w-48" placeholder="请输入邮箱" value=""></input> */}
+        <input type="text" className="m-4 text-white rounded-xl p-2 border-solid border-2 border-[#323437] hover:border-[#6bc001] focus:border-[#6bc001] bg-transparent" placeholder="请输入邮箱" ref={$email}></input>
+        <div className='flex ml-auto mt-auto gap-3 text-white'>
+          <button className=''>取消</button>
+          <button className='rounded-md bg-[#6bc001] p-1' onClick={onClickSubmit}>发送好友请求</button>
+        </div>
       </div>
     </div>
   )
