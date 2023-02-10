bb4399c0b46d27a43af35fa33477a2b6889a3f6f
sawyerrick <1103031078@qq.com>
2023-02-10 13:02:31 +0800

    online panel

--- src/pages/home/HomeSideBar.jsx
+++ src/pages/home/HomeSideBar.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 export default function HomeSideBar () {
   useState(0)
   return (
-    <div className="shrink-0 flex flex-col h-full w-64 border-r-[1px] border-solid border-r-[#323437]">
+    <div className="p-2 shrink-0 flex flex-col h-full w-64 border-r-[1px] border-solid border-r-[#323437]">
       <UserInfoPanel />
       <FriendPanel />
       <InboxPanel />
@@ -14,14 +14,14 @@ export default function HomeSideBar () {
 
 function UserInfoPanel () {
   return (
-    <div className="m-2"><p className="text-[#e6eaf0] font-bold">Sawyer</p></div>
+    <div className=""><p className="text-[#e6eaf0] font-bold">Sawyer</p></div>
   )
 }
 
 function FriendPanel () {
   return (
-    <div className='mt-2'>
-      <p className='text-[#e6eaf0] text-sm m-2'>好友</p>
+    <div className='mt-3'>
+      <p className='text-[#e6eaf0] text-sm mt-2'>好友</p>
       <div className='ml-4'>
         <FriendSelection name='在线' to='/home/friend/online'/>
         <FriendSelection name='全部' to='/home/friend/all'/>
@@ -43,8 +43,8 @@ function FriendSelection ({ icon, name, cnt, to }) {
 
 function InboxPanel () {
   return (
-    <div className='mt-2'>
-      <p className='text-[#e6eaf0] text-sm m-2'>收件箱</p>
+    <div className='mt-3'>
+      <p className='text-[#e6eaf0] text-sm mt-2'>收件箱</p>
       <div className='ml-4'>
         <InboxSelection name='系统通知'/>
         <InboxSelection name='私信'/>
--- src/pages/home/friend/all/index.jsx
+++ src/pages/home/friend/all/index.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react'
 import { HomeDashboard } from 'src/pages/home/index'
 
 export default function All () {
+  const [openAddFriend, setOpenAddFriend] = useState(false)
+  console.log(openAddFriend)
+
   return (
     <HomeDashboard>
-      <h1 >Hello All....</h1>
+      <div className='p-5'>
+        <div className='flex'>
+          <h1 className='text-white'>全部&nbsp;-&nbsp;0</h1>
+          <button className='ml-5 rounded-md bg-[#6bc001] text-white px-2' onClick={() => setOpenAddFriend(true)}>添加好友</button>
+        </div>
+        {/* {openAddFriend ? <AddFriendWindow/> : ''} */}
+        <div className='flex p-3 flex-wrap items-start content-start'>
+          <FriendCard />
+          <FriendCard />
+          <FriendCard />
+          <FriendCard />
+          <FriendCard />
+          <FriendCard />
+          <FriendCard />
+          <FriendCard />
+        </div>
+      </div>
     </HomeDashboard>
+  )
+}
+
+function AddFriendWindow () {
+  return (
+    <div className='fixed w-64 h-96 mx-auto bg-white'>
+
+    </div>
+  )
+}
+
+function FriendCard () {
+  return (
+    <div className='flex flex-col h-32 w-72 m-3 p-4 bg-[#36383e] rounded-3xl'>
+      <div className='flex items-center'>
+        <div className='w-14 h-14 bg-white rounded-full'></div>
+        <div><p className='ml-3 text-[#e6eaf0]'>Sawyer@gmail.com</p></div>
+      </div>
+      {/* 两个icon */}
+    </div>
   )
 }
