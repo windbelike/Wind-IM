febe465f4ff53c93ec5ca94eed657efe81dbc8ad
sawyerrick <1103031078@qq.com>
2023-02-14 13:20:54 +0800

    private msg input div

--- packages/web/src/pages/home/inbox/[msgId].jsx
+++ packages/web/src/pages/home/inbox/[msgId].jsx
@@ -2,11 +2,25 @@ import { useRouter } from 'next/router'
 import { HomeDashboard } from '..'
 import { io } from 'socket.io-client'
 import { useEffect, useState } from 'react'
-import getCookie from '@/components/CookieUtils'
 
 export default function Inbox () {
   const router = useRouter()
   const { msgId } = router.query
+  useWs(msgId)
+
+  return (
+    <HomeDashboard>
+      <div className='p-3 w-full h-full flex flex-col'>
+        <div className='h-24 border-b-[1px] border-solid border-b-[#323437] text-white'>Head</div>
+        <p className='text-white'>msgId: {msgId}</p>
+        <div></div>
+        <input className='break-all mt-auto h-14  px-10 py-4 rounded-2xl text-white bg-[#36383e]' contentEditable></input>
+      </div>
+    </HomeDashboard>
+  )
+}
+
+function useWs (msgId) {
   useEffect(() => {
     let socket
     if (msgId) {
@@ -25,13 +39,4 @@ export default function Inbox () {
     }
     return () => { socket && socket.disconnect() }
   }, [msgId])
-
-  return (
-    <HomeDashboard>
-      <div className='p-5'>
-        <p className='text-white'>msgId: {msgId}</p>
-
-      </div>
-    </HomeDashboard>
-  )
 }
