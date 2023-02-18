727b776b94012eae4675a2e7a30bac07e3b97a5f
windbelike <1103031078@qq.com>
2023-02-19 00:27:39 +0800

    gitignore

--- packages/server/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var wsService_1 = __importDefault(require("./service/ws/wsService"));
-wsService_1["default"].listen(2000, function () {
-    console.log('listening on *:2000');
-});
--- packages/server/dist/service/sum.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function sum(a, b) {
-    return a + b;
-}
-exports["default"] = sum;
