8dc99cfb93a6c3370e60c415e7cc6dcea269c8e4
windbelike <1103031078@qq.com>
2023-02-23 21:22:55 +0800

    optimize register

--- packages/web/src/pages/entry/signup/index.jsx
+++ packages/web/src/pages/entry/signup/index.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios'
+import Link from 'next/link'
 import { useRef, useState } from 'react'
 import { useMutation } from 'react-query'
 
 // Custom nested route for _app.js
 SignUpForm.isEntry = true
 
-async function createAccount ({ email, pwd }) {
+async function createAccount ({ email, username, pwd }) {
   await axios.post('/api/signup', {
     email, pwd
   })
@@ -14,6 +15,7 @@ async function createAccount ({ email, pwd }) {
 export default function SignUpForm () {
   // useRef don't cause a rerender
   const $email = useRef()
+  const $username = useRef()
   const $pwd = useRef()
 
   // prisma.user.findMany()
@@ -25,6 +27,7 @@ export default function SignUpForm () {
 
   function onClickCreateAccount () {
     const email = $email.current.value
+    const username = $username.current.value
     const pwd = $pwd.current.value
     console.log('email:' + email)
     console.log('pwd:' + pwd)
@@ -33,15 +36,21 @@ export default function SignUpForm () {
 
   return (
     <>
-      <h1>SignUp Page</h1>
-      <div className='flex h-full justify-center items-center'>
-        <div className='flex flex-col bg-gray-100 p-8 rounded-lg'>
-          <label htmlFor="emailInput">Email: </label>
-          <input ref={$email} id="emailInput" type="text" name="text"/>
-          <br/>
-          <label htmlFor="pwdInput">Password: </label>
-          <input ref={$pwd} className='rounded-sm' id="pwdInput" type="password" name="text"/>
-          <button disabled={createAccountMutation.isLoading} className='h-10 rounded-full bg-sky-500 text-white hover:bg-sky-600 mt-7 mx-5 px-5 shrink-0 focus:ring focus:ring-sky-300 active:bg-sky-700 focus:outline-none' onClick={onClickCreateAccount}>Sign Up</button>
+      <div className='flex h-screen w-screen justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500'>
+        <div className='flex flex-col h-[502px] w-[408px] p-8 bg-[#313338]  text-white'>
+          <h1 className=' text-center text-2xl'>Create an account</h1>
+          <div className='flex flex-col pt-5'>
+            <p className='text-[#b8b9bf] font-bold text-sm mt-5 mb-1'>EMAIL</p>
+            <input className='bg-[#1e1f22] p-2' ref={$email} type="text"/>
+            <p className='text-[#b8b9bf] font-bold text-sm mt-5 mb-1'>USERNAME</p>
+            <input className='bg-[#1e1f22] p-2' ref={$username} type="text"/>
+            <p className='text-[#b8b9bf] font-bold text-sm mt-5 mb-1'>PASSWORD</p>
+            <input className='bg-[#1e1f22] p-2' ref={$pwd} type="password"/>
+          </div>
+          <button disabled={createAccountMutation.isLoading} className='h-10 rounded-md bg-[#6161f1] mt-7 shrink-0' onClick={onClickCreateAccount}>Continue</button>
+          <Link href='/entry/login'>
+            <a className='mt-3 underline text-blue-400'>Already have an account?</a>
+          </Link>
         </div>
       </div>
     </>
