161ccdb294f00632c9144d3edd7e93299433fcbd
windbelike <1103031078@qq.com>
2023-04-02 20:15:36 +0800

    根元素滚动条处理Sidebar提示overlow时不显示的问题

--- packages/web/src/pages/SideBar.jsx
+++ packages/web/src/pages/SideBar.jsx
@@ -40,10 +40,12 @@ export default function SideBar () {
   }
 
   return (
-    <div className='overflow-x-visible'>
+    <div>
       {addServerFlag && <AddAChannelBg id={bgElementId} onClickCloseAddAServer={onClickCloseAddAServer} />}
       {/* todo 解决overflow icon不展示的问题 && overflow-hidden icon的tooltip不展示 */}
-      <div className='h-screen w-[72px] overflow-hidden
+      <div className='
+      h-full
+      w-[72px]
       flex flex-col items-center
       bg-[#17181a] text-white shadow-lg'>
         {/* LOGO */}
@@ -51,16 +53,14 @@ export default function SideBar () {
           <img className="h-12 w-12 rounded-full" src="https://avatars.githubusercontent.com/u/33996345?v=4" alt="ChitChat Logo" />
         </div> */}
         {/* Main Functions */}
-        <div className='mt-2'>
-          <SideBarIcon linkTo='/' text='Home' icon={<AiOutlineHome size="28" />} />
-          <div className=' w-[40px] h-[1px] bg-[#2f2f30] mx-4 my-2'></div>
-          <ChannelIconList data={data}/>
-          <AddAChannelIcon />
-          <SideBarIcon linkTo='/msg' text='Messages' icon={<AiOutlineMessage size="28" />} />
-          <SideBarIcon linkTo='/user/profile' text='Profile' icon={<AiOutlineUser size="28" />} />
-          {/* <SideBarIcon linkTo='/entry/login' text='Profile' icon={<AiOutlineUser size="28" />} /> */}
-          <SideBarIcon linkTo='/entry/logout' text='Logout' icon={<AiOutlineLogin size="28" />} />
-        </div>
+        <SideBarIcon linkTo='/' text='Home' icon={<AiOutlineHome size="28" />} />
+        <div className='shrink-0 w-[40px] h-[1px] bg-[#2f2f30] mx-4 my-2'></div>
+        <ChannelIconList data={data}/>
+        <AddAChannelIcon />
+        <SideBarIcon linkTo='/msg' text='Messages' icon={<AiOutlineMessage size="28" />} />
+        <SideBarIcon linkTo='/user/profile' text='Profile' icon={<AiOutlineUser size="28" />} />
+        {/* <SideBarIcon linkTo='/entry/login' text='Profile' icon={<AiOutlineUser size="28" />} /> */}
+        <SideBarIcon linkTo='/entry/logout' text='Logout' icon={<AiOutlineLogin size="28" />} />
         <div className="mt-auto">
           <SideBarIcon linkTo='/settings' text="Settings" icon={<AiOutlineSetting size="28" />} />
         </div>
@@ -88,7 +88,7 @@ function ChannelIconList ({ data }) {
 
 const SideBarIcon = ({ icon, text = 'tooltip 💡', linkTo = '/' }) => (
   <Link href={linkTo} >
-    <div className="sidebar-icon group">
+    <div className="sidebar-icon group overflow-x-visible">
       {icon}
       {/* Styling based on parent state (group-{modifier}) */}
       <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
