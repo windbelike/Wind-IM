31eb060184e0555f0deb0e9ccf6ab77ba460ccc6
windbelike <1103031078@qq.com>
2023-05-14 20:01:40 +0800

    add to fix

--- packages/web/src/pages/home/HomeLayout.jsx
+++ packages/web/src/pages/home/HomeLayout.jsx
@@ -3,12 +3,17 @@ import HomeSideBar from './HomeSideBar'
 
 export default function HomeLayout ({ children }) {
   const sideBarActiveState = useState('')
+  // const Children = children
+  // const c = <Children sideBarActiveState={sideBarActiveState} />
   return (
     <>
       <div className='h-full w-full bg-[#25272a] flex'>
         <HomeSideBar />
+        {/* todo pass sideBarActiveState to children as props */}
         {children}
       </div>
     </>
   )
 }
+
+// const getPage = (page) => page
