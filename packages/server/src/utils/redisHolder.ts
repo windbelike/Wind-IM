2403c5bfbab890fefba3f7d0896f149939d63562
windbelike <1103031078@qq.com>
2023-05-21 23:07:09 +0800

    update: docker-compose file

--- packages/server/src/utils/redisHolder.ts
+++ packages/server/src/utils/redisHolder.ts
@@ -2,8 +2,24 @@ import { Redis } from 'ioredis'
 
 let redis:Redis
 
+const devRedisUrl = ''
+const betaRedisUrl = 'redis://redis:6379'
+const prodRedisUrl = ''
+
 try {
-  redis = new Redis()
+  console.log('WINDIM_ENV: ' + process.env.WINDIM_ENV)
+  let redisUrl = ''
+  if (process.env.WINDIM_ENV === 'dev') {
+    redisUrl = devRedisUrl
+  } else if (process.env.WINDIM_ENV === 'beta') {
+    redisUrl = betaRedisUrl
+  } else if (process.env.WINDIM_ENV === 'prod') {
+    redisUrl = prodRedisUrl
+  } else {
+    redisUrl = devRedisUrl
+  }
+
+  redis = new Redis(redisUrl)
 } catch (e) {
   console.error(e)
 }
